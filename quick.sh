24c1c04009fd9626bc49a47ec3bfa7daf1db1e0f
Soham-Chatt <113625563+Soham-Chatt@users.noreply.github.com>
2023-05-18 21:27:50 +0200

    Scripts improvements

--- quick.sh
+++ quick.sh
@@ -11,40 +11,9 @@ function error_exit {
 # Set the trap to call the error_exit function on error
 trap error_exit ERR
 
-# Build the program
-echo "Quick run? (y/n)"
-read -r quick
-if [ "$quick" = "y" ]
-then
-  echo "You selected yes. Deleting all previous output files and building the program."
-  make clean
-  make
-elif [ "$quick" = "n" ]
-then
-  echo "You selected no. Building the program normally."
-  make
-else
-  echo "Invalid input. Exiting the script."
-  exit 2
-fi
-
-# Select the type and run the program
-echo "Select the type: Mandelbrot (1) or Buddhabrot (2)"
-read -r choice
-if [ "$choice" -eq 1 ]
-then
-  echo "You selected Mandelbrot. Starting the program."
-  ./mandelbrot
-elif [ "$choice" -eq 2 ]
-then
-  echo "You selected Buddhabrot. Starting the program."
-  ./buddhabrot
-else
-  echo "Invalid input. Exiting the script."
-  exit 2
-fi
-
-echo "Program completed. Converting the PMM to TIFF."
+make clean
+make
+./mandelbrot
 
 # Get the latest ppm file and convert it to tiff
 latest_ppm=$(find . -maxdepth 1 -name '*.ppm' -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
