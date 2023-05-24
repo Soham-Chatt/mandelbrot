a0f9a826a2689fd768e2c44e9f1f21feec6eb5b2
Soham-Chatt <113625563+Soham-Chatt@users.noreply.github.com>
2023-05-24 14:01:04 +0200

    Script to send mail

--- sendMail.sh
+++ sendMail.sh
@@ -3,7 +3,7 @@ pid=$(pidof mandelbrot)
 
 while :; do
     # Check if process is running
-    if ! ps -p $pid > /dev/null; then
+    if ! ps -p "$pid" > /dev/null; then
         echo "Process $pid no longer exists. Exiting script."
         # Send an email when process is no longer running
         echo "Process $pid is no longer running." | mail -s "Mandelbrot rendering has finished" neel.delft@gmail.com
