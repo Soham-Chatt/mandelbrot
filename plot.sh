5ed6a29b141d0cd8bfc02c9fdbd361c0ae3ae01b
Soham-Chatt <113625563+Soham-Chatt@users.noreply.github.com>
2023-05-24 20:58:35 +0200

    Script improvements

--- plot.sh
+++ plot.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-set -e  # Exit on error
-
-# Define a function that will run when an error occurs
-function error_exit {
-    echo "An error occurred. Exiting the script."
-    exit 1
-}
-
-# Set the trap to call the error_exit function on error
-trap error_exit ERR
-
 # Create the stripped down file
 echo "Stripping down the file"
 sed '1,2d' nohup.out > processing.txt
