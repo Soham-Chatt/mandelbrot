ee49436a31c77453f06b0f8eadd02ef8f6354e1b
Soham-Chatt <113625563+Soham-Chatt@users.noreply.github.com>
2023-05-24 17:52:31 +0200

    Comments

--- plot.sh
+++ plot.sh
@@ -11,8 +11,12 @@ function error_exit {
 # Set the trap to call the error_exit function on error
 trap error_exit ERR
 
+# Create the stripped down file
+echo "Stripping down the file"
 sed '1,2d' nohup.out > processing.txt
 
+# Run the script
+echo "Running the script"
 source venv/bin/activate
 python3 plot.py
 deactivate
