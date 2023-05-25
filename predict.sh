3c1b43a302b2fec9ddfa8d68ae38a71ecf70a696
Soham-Chatt <113625563+Soham-Chatt@users.noreply.github.com>
2023-05-25 16:47:46 +0200

    Script change

--- predict.sh
+++ predict.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Create the stripped down file
-echo "Stripping down the file"
-awk '/0/,0' nohup.out > processing.txt
-
 # Run the script
 echo "Running the script"
 source venv/bin/activate
